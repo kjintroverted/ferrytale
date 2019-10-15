c50a63b9cad8d5a7aab34b5f5887db25f3eb387e
Williamrobert Green <wkgreen13@gmail.com>
2019-10-15 15:26:26 -0400

    moves font to root style

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -29,13 +29,11 @@ const Container = styled.div`
 `
 
 const Main = styled.h1`
-  font-family: "Raleway";
   margin: 10px 0px;
   font-size: 4em;
 `
 
 const Text = styled.h3`
-  font-family: "Raleway";
   margin: 10px 0px;
   font-size: 1.2em;
 `
