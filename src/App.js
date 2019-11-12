9329cc2582b7d56011e01248406fa70b301db247
Williamrobert Green <wkgreen13@gmail.com>
2019-11-12 15:14:31 -0500

    adds content update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
 import styled from 'styled-components';
 
 import './App.css';
@@ -25,11 +25,29 @@ function App() {
       <IGBackground
         username="ferrytalecreative"
         filterOpts={ ["to bottom right", "teal", "blue", "purple"] }
-        offset={ 65 }
+        offset={ 55 }
       />
       <Banner>
-
+        <Feature>
+          <Billboard>
+            <h1>The store is <b>LIVE!</b></h1>
+            <h3>Some subtitle.</h3>
+          </Billboard>
+          <Details>
+            <p>Butcher poke coloring book, authentic mixtape lumbersexual franzen food truck fanny pack single-origin coffee skateboard. Butcher lo-fi enamel pin gastropub, plaid lomo poutine taiyaki chicharrones hell of cray. 8-bit hell of lo-fi 3 wolf moon heirloom fixie typewriter mustache jianbing, DIY gentrify chambray.</p>
+            <Button variant="contained" color="secondary" href="https://etsy.com/shop/FerryTaleCreative">
+              Shop now
+            </Button>
+          </Details>
+        </Feature>
       </Banner>
+      <Footer>
+        <Contact>
+          <h4>Connect with us.</h4>
+          <p>Follow us on Instagram <a href="https://www.instagram.com/ferrytalecreative/">@FerryTaleCreative</a></p>
+          <p>Have a question? <a href="https://forms.gle/t6NSqEZNVMPoU5Lm7">Ask us here!</a></p>
+        </Contact>
+      </Footer>
     </Content>
   );
 }
@@ -39,7 +57,53 @@ export default App;
 const Content = styled.div``
 
 const Banner = styled.div`
-  position:
+  height: 95vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Feature = styled.div`
+  width: 90%;
+  max-width: 700px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+`
+
+const Billboard = styled.div`
+  color: white;
+  background: rgba(0, 0, 0, .2);
+  padding-left: 10px;
+  padding-bottom: 10px;
+  border-left: white solid;
+`
+
+const Details = styled.div`
+  width: 90%;
+  max-width: 600px;
+  color: white;
+  margin-top: 50px;
+  text-align: justify;
+`
+
+const Footer = styled.div`
+  min-height: 300px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  & h4 {
+    margin: 15px 0px;
+  }
+  & p {
+    margin: 5px 0px;
+  }
+`
+
+const Contact = styled.div`
+  width: 90%;
+  max-width: 700px;
 `
 
 const Spacer = styled.span`
